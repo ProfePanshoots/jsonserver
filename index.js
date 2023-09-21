d1676163791986c68efe13d34aa34824c99b9961
Francisco Juillet <franciscojuilletferj@gmail.com>
2023-09-21 08:29:16 -0300

    Inicio server

---
+++ index.js
@@ -0,0 +1,10 @@
+const jsonServer = require("json-server"); // IMPORTA JSON-SERVER
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001; // SE PUEDE USAR CUALQUIER PUERTO
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
